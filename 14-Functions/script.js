6765201f82f5bd6c3fe666ee8574b427c108c951
RustamMamedoff <rustammamedov6363@gmail.com>
2025-04-03 20:58:49 +0400

    js hell olundu

---
+++ 14-Functions/script.js
@@ -0,0 +1,146 @@
+//let num1 = 10;
+//let num2 = 5;
+
+//function Math(a, b) {
+   // console.log(a + b);
+   // console.log(a - b);
+   //console.log(a * b);
+   //console.log(a / b);
+//}
+
+//Math(num1, num2);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+//const numbers = [14, 20, 35, 40, 57, 60, 100];
+
+//function cutvetek(...nums) {
+   // const EvenNumbers = [];
+   // const OddNumbers = [];
+
+   // for (let num of nums) {
+     //  if (num % 2 === 0) {
+      //      EvenNumbers.push(num); 
+      // } else {
+      //      OddNumbers.push(num); 
+      //  }
+  //  }
+
+  // console.log(EvenNumbers);
+  //  console.log(OddNumbers);
+//}
+
+//cutvetek(...numbers);
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+//const numbers = [14, 20, 35, 40, 57, 60, 100];
+
+//function elementcemi(nums) {
+   // let sum = 0;
+
+ //   for (let num of nums) {
+    //    if (num % 5 === 0 && num % 4 === 0) {  
+       //     sum += num;  
+      //  }
+  //  }
+   // return sum;
+//}
+
+//const result = elementcemi(numbers);
+//console.log(result);
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
+//function cumleuzunlugu(sentence) {
+   // return sentence.length; 
+//}
+
+
+//const sentence = "Sabah oldu gettim ise axsam oldu qayittim isden.";
+
+
+//const lengthofsentence = cumleuzunlugu(sentence);
+
+
+//console.log(lengthofsentence);
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+//function numbersl(num) {
+  // if (num <= 1) return false;
+   //for (let i = 2; i < num; i++) {
+   //  if (num % i === 0) return false;
+ //  }
+  // return true;
+//}
+ 
+ //const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15];
+ 
+ //let Aboundant = 0;
+ //let Deficient = 0;
+ 
+ //for (let i = 0; i < numbers.length; i++) {
+  // if (numbersl(numbers[i])) {
+   //  Aboundant++;
+  // } else {
+ //    Deficient++;
+ //  }
+ //}
+ //
+ //console.log({Aboundant , Deficient});
+ 
+
+ 
+
+////////////////////////////////////////////////////////////////////////////////////
+//function squarenumbers(arr) {
+ //  const squaredArr = [];
+ //  for (let i = 0; i < arr.length; i++) {
+   //    squaredArr.push(arr[i] ** 2);  
+ //  }
+ //  return squaredArr;
+//}
+
+//const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//const squaredNumbers = squarenumbers(numbers);
+
+
+//console.log(numbers);
+//console.log(squaredNumbers);
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
