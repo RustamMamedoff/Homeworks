85bacb025606224e39be86a9b6dcdc283d24d67d
RustamMamedoff <rustammamedov6363@gmail.com>
2025-04-08 21:48:03 +0400

    javascript yazildi

---
+++ 17-DOMSelectorStyleNavigateInteractiveElements/card.js
@@ -0,0 +1,108 @@
+// const style = document.createElement("style");
+//         style.innerHTML = `
+//             .card {
+//     border: 2px black;
+//     display: flex;
+//     justify-content: center;
+//   }
+  
+//   .main-image {
+//     display: flex;
+//   }
+  
+//   .main-image img {
+//     height: 250px;
+//     width: 400px;
+//     position: relative;
+//     top: 30px;
+//   }
+  
+//   .cost {
+//      display: flex;
+//      position: relative;
+//      top: 300px;
+//      right: 390px;
+//   }
+  
+//   .cost h3 {
+//     color: gray;
+//     font-size: 15px;
+//   }
+  
+//   .cost h2 {
+//     color: black;
+//     position: relative;
+//     top: 30px;
+//     right: 195px;
+//   }
+  
+//   .cost p {
+//     color: black;
+//     position: relative;
+//     top: 70px;
+//     right: 290px;
+//   }
+  
+//   .icons {
+//     display: flex;
+//     gap: 10px;
+//   }
+  
+//   .icons i, p {
+//     position: relative;
+//     top: 420px;
+//     right: 700px;
+//   }
+
+//   .super {
+//     position: relative;
+//     right: 120px;
+//     display: flex;
+//     gap: 10px;
+//   }
+
+//   .super p {
+//    top:-5px;
+//    top:402px;
+//   }
+  
+//   .supert {
+//     display: flex;
+//     gap: 10px;
+//   }
+
+//   .supert p {
+//   top:-5px;
+//    top:402px;
+//   }
+  
+//   .futer h2 {
+//     position: relative;
+//     top: 460px;
+//     right: 1020px;
+//   }
+  
+  
+//   .futer img {
+//     height: 50px;
+//     width: 50px;
+//     border-radius: 50px;
+//     position: relative;
+//     top: 465px ;
+//     right: 1020px;
+//   }
+  
+//   .futer h3 {
+//     position: relative;
+//     top: 390px;
+//     right: 900px;
+//   }
+  
+//   .futer p {
+//     position: relative;
+//     right: 900px;
+//     top: -5px;
+//     top: 380px;
+//   }            
+//         `;
+//         document.head.appendChild(style);
---
+++ 17-DOMSelectorStyleNavigateInteractiveElements/script.js
@@ -0,0 +1,96 @@
+//const hello = document.querySelectorAll(".heart");
+
+//for (let i = 0; i < hello.length; i++) {
+  //const card = hello[i].closest(".card");
+  
+  // const title = card.querySelector(".cardTitle").textContent; 
+  //const description = card.querySelector(".cardText").textContent; 
+  // const price = card.querySelector(".cardPrice").textContent; 
+  
+ //console.log("Product :", {
+  //  title: title,
+   // description: description,
+   // price: price
+  //});
+//}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// const style = document.createElement("style");
+// style.innerHTML = `
+//   .heart {
+//     display: inline-flex;
+//     align-items: center;
+//     justify-content: center;
+//     width: 30px;
+//     height: 30px;
+//     border-radius: 50%;
+//     background-color: silver;
+//     position: absolute;
+//     top: 15px;
+//     right: 15px;
+//     cursor: pointer;
+//   }
+
+//   .image {
+//     width: 100%;
+//     height: 300px;
+//     position: relative;
+//     display: flex;
+//     justify-content: center;
+//   }
+
+//   img {
+//     width: 25%;
+//     height: 80%;
+//     border-radius: 10px;
+//   }
+
+//   .cardContent {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     gap: 10px;
+//     margin-top: 20px;
+//     color: black;
+//   }
+
+//   .cardPrice {
+//     display: inline-block;
+//     padding: 5px;
+//     border-radius: 5px;
+//     background-color: black;
+//     color:#fff;
+//   }
+
+//   .shopBtn {
+//     width: 20%;
+//     padding: 10px;
+//     background-color: blue;
+//     border: none;
+//     cursor: pointer;
+//     color: white;
+//     text-transform: uppercase;
+//     border-radius: 5px;
+//     font-weight: bold;
+//   }
+// `;
+// document.head.appendChild(style);
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// document.querySelector(".cardTitle").textContent = "Shopping";
+
+// document.querySelector(".cardText").textContent = "welcome to our company";
+
+// document.querySelector(".cardPrice").textContent = "200 000 Azn";
+
+// document.querySelector(".image img").src = "https://picsum.photos/400/500"; 
+
+// document.querySelector(".shopBtn").textContent = "ADD";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
