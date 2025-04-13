a534b84abb7afee78cc93f17f662f2fb22375037
RustamMamedoff <rustammamedov6363@gmail.com>
2025-04-13 20:41:43 +0400

    javascript yazildi

---
+++ 20-Wishlist/Assets/javascript/login.js
@@ -0,0 +1,32 @@
+// const loginForm = document.getElementById("login-form");
+
+//   loginForm.addEventListener("submit", function (e) {
+//     e.preventDefault();
+
+//     const inputUsername = document.getElementById("username").value.trim();
+//     const inputPassword = document.getElementById("password").value;
+//     const correctUsername = "admin";
+//     const correctPassword = "1234";
+
+//     if (inputUsername === correctUsername && inputPassword === correctPassword) {
+//       window.location.href = "wishlist.html";
+//     } else {
+//       alert("incorrect username or password");
+//     }
+//   });
+
+
+
+
+//  document.getElementById("logoutBtn").addEventListener("click", function () {
+//     Toastify({
+//       text: "you want to logout? click here!",
+//       duration: 5000,
+//       close: true,
+//       gravity: "top", 
+//       position: "right",
+//       backgroundColor: "orange",
+//       onClick: function () {
+//       }
+//     }).showToast();
+//   });
---
+++ 20-Wishlist/Assets/javascript/script.js
@@ -0,0 +1,11 @@
+// const hearts = document.querySelectorAll('.heart-icon');
+//   hearts.forEach(function(heart) {
+//     heart.addEventListener("click", function() {
+//       if (heart.classList.contains("selected")) {
+//         heart.classList.remove("selected");
+//       } else {
+//         heart.classList.add("selected");
+//       }
+//     });
+//   });
+
